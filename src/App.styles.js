7b3cc16c4e29ea8586bbf9445644d31f3f1eb807
Nesquiktatar <coding96558843330@gmail.com>
2021-04-19 11:25:48 +0300

    'backgroundDisplay'

--- src/App.styles.js
+++ src/App.styles.js
@@ -1,9 +1,10 @@
 import styled from '@emotion/styled';
 
 export const App = styled.div`
-    background  : ${p => p.appPage.themes.dark.black};
-    height: 100vh;
+   background  : ${p => p.appPage.themes.dark.black};
     display: flex;
+    height:auto;
+    min-height: 100vh   ;
 `
 
 export const Header = styled.div`
